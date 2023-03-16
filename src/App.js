640165b733f09c8ca398d0d6ef517118f7f0e5ff
FlamboyantFate <105473396+FlamboyantFate@users.noreply.github.com>
2023-03-16 22:48:43 +0545

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import Navbar from "./components/navbar";
 function App() {
-  return <div className="App"></div>;
+  return <div>
+
+<Navbar/>
+
+  </div>;
 }
 
 export default App;
