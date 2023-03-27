b897139407d6fc3b0d42ec7d0d3f697304228f06
nirajan1111 <110184689+nirajan1111@users.noreply.github.com>
2023-03-28 00:41:48 +0545

    card and sidebar arrangement fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Cards from "./components/Cards";
 import Sidebar from "./components/sidebar";
 function App() {
   return (
-    <div>
-      <Sidebar />
+    <div className="body_container">
+      <div className="sidebar_container">
+        <Sidebar />
+      </div>
+      <div className="card_container">
+        <Cards />
+      </div>
     </div>
   );
 }
